1b4c6b1dbb7dc3a5dc564426e169624c2b255b14
blandflakes <xclite@gmail.com>
2019-07-05 14:23:52 -0400

    Initial implementation.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Vita
+
+This repository is a declaration of my programming skills, experience, and plans.
+
+`vita.edn` is essentially a computer-readable data structure containing a hopefully up-to-date
+representation of my skills.
+
+`next.edn` is conceptually a set of future plans, each key of which may eventually be merged into
+`vita.edn`. It is a file tracking my interests; I may not actively be working on any of those items
+directly.
+
+`vita.clj` generates HTML and PDF versions of my resume from `vita.edn`.
---
+++ src/vita.clj
@@ -0,0 +1,52 @@
+(ns vita
+  (:require [clojure.string :as str]
+            [clojure.edn :as edn]
+            [clj-htmltopdf.core :refer [->pdf]])
+  (:use [hiccup.page]
+        [hiccup.element :refer [link-to]]))
+
+(defn #^:private vita->html
+  []
+  (let [vita (edn/read-string (slurp "vita.edn"))]
+    (html5
+      {:lang "en"}
+      (include-css "style.css")
+      [:head [:title (str (:name vita) ": " (:title vita))]]
+      [:body
+       [:h1 (str (:name vita) " - " (:title vita))]
+       [:p (:summary vita)]
+       [:span (interpose " | " (map (fn [[name url]] (link-to url name)) (:personas vita)))]
+       [:div
+        [:h2 "Stack"]
+        [:h3 "Languages"]
+        [:p (str/join ", " (get-in vita [:stack :languages :core]))]
+        [:h3 "Tools"]
+        [:p (str/join ", " (get-in vita [:stack :tools]))]
+        [:h3 "Patterns"]
+        [:p (str/join ", " (:patterns vita))]]
+       [:div
+        [:h2 "Experience"]
+        (for [company (:experience vita)]
+          [:div
+           [:h3 (link-to (:url company) (:company company))]
+           (for [role (:roles company)]
+             [:div
+              [:h4 (:title role)]
+              [:p (str (:started role) " - " (get role :ended "present"))]
+              [:ul
+               (for [responsibility (:responsibilities role)]
+                 [:li responsibility])]])])]
+       [:div
+        [:h2 "Education"]
+        (for [program (:education vita)]
+          [:div
+           [:h3 (str (:degree program) " " (:major program) ", " (:graduated program))]
+           [:p (str (:institution program) ", " (:location program))]
+           [:p (str "GPA: " (:gpa program)) (if-let [major-gpa (:major-gpa program)] (str " Major GPA: " major-gpa))]])]])))
+
+(defn -main
+  "Generates HTML and PDF versions of vita.edn"
+  []
+  (let [rendered (vita->html)]
+    (spit "index.html" rendered)
+    (->pdf rendered "resume.pdf")))
